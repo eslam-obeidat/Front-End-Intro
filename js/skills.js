b2a24b1d3a9669040ac484656dc55394bc87702a
eslam-obeidat <146520433+eslam-obeidat@users.noreply.github.com>
2023-11-03 23:35:58 -0800

    .

--- js/skills.js
+++ js/skills.js
@@ -1,1 +1,19 @@
-function
+// alert('test
+
+
+
+var body = document.getElementsByTagName('body');
+//  console.log(body[0].style.backgroundColor);
+
+
+function changeBackground(){
+    // body[0].style.backgroundColor='red'
+
+
+    if(body[0].style.background=='red'){
+    body[0].style.background='pink';
+    
+}else{
+    body[0].style.background='red'  
+}
+}
